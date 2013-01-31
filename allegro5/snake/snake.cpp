e496d15b40bfe331a0ddb333769894317be8e9a5
Andrzej Nagórko <amn@mimuw.edu.pl>
2013-01-31 16:29:23 +0100

    Uproszczona wersja węża.

--- allegro5/snake/snake.cpp
+++ allegro5/snake/snake.cpp
@@ -1,86 +1,108 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 
-const float FPS = 60;
-const int SCREEN_W = 640;
-const int SCREEN_H = 480;
+#include <iostream>
+using namespace std;
 
-enum MYKEYS {
-    KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
-};
+const float FPS = 60;       // Frames Per Second
+const int screen_w = 640;   // szerokość ekranu (screen width)
+const int screen_h = 480;   // wysokość ekranu (screen height)
+const int segment = 5;      // rozmiar segmentu węża
 
-struct punkt {
-    int x;
-    int y;
-};
+bool key[ALLEGRO_KEY_MAX];  // wciśnięte klawisze
 
-struct waz {
-    struct punkt cielsko[10000];
-    
-    int dlugosc;
-    
-    struct punkt glowa;
-};
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+ALLEGRO_TIMER *timer = NULL;
 
-struct waz w;
-
-const int segment = 5;
-
-int main(int argc, char ** argv){
-    ALLEGRO_DISPLAY *display = NULL;
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    ALLEGRO_TIMER *timer = NULL;
- 
-    bool key[4] = { false, false, false, false };
-    bool redraw = true;
-    bool doexit = false;
-  
+int init()
+{
     if(!al_init()) {
-       fprintf(stderr, "failed to initialize allegro!\n");
-       return -1;
+        cerr << "Błąd podczas inicjalizacji allegro." << endl;
+        return -1;
     }
   
     if(!al_install_keyboard()) {
-       fprintf(stderr, "failed to initialize the keyboard!\n");
-       return -1;
+        cerr << "Błąd podczas inicjalizacji klawiatury." << endl;
+        return -1;
     }
   
     timer = al_create_timer(1.0 / FPS);
     if(!timer) {
-       fprintf(stderr, "failed to create timer!\n");
-       return -1;
+        cerr << "Błąd podczas inicjalizacji zegara." << endl;
+        return -1;
     }
  
-    display = al_create_display(SCREEN_W, SCREEN_H);
-
-     if(!display) {
-        fprintf(stderr, "failed to create display!\n");
+    display = al_create_display(screen_w, screen_h);
+    if(!display) {
+        cerr << "Błąd podczas inicjalizacji ekranu." << endl;
         al_destroy_timer(timer);
         return -1;
-     }
+    }
 
-    al_set_target_bitmap(al_get_backbuffer(display));
-  
     event_queue = al_create_event_queue();
     if(!event_queue) {
-       fprintf(stderr, "failed to create event_queue!\n");
-       al_destroy_display(display);
-       al_destroy_timer(timer);
-       return -1;
+        cerr << "Błąd podczas inicjalizacji kolejki zdarzeń." << endl;
+        al_destroy_display(display);
+        al_destroy_timer(timer);
+        return -1;
     }
   
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-  
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
-  
+    al_register_event_source(event_queue, al_get_display_event_source(display));  
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));  
     al_register_event_source(event_queue, al_get_keyboard_event_source());
-  
     al_clear_to_color(al_map_rgb(0,0,0));
   
-    al_flip_display();
-  
+    al_flip_display();  
     al_start_timer(timer);
+
+    return 0;
+}
+
+/****************************************
+ * Tu rozpoczyna się istotna część kodu *
+ ****************************************/
+ 
+//
+// struktura reprezentująca punkt - parę współrzędnych
+//
+
+struct punkt {
+    float x;
+    float y;
+};
+
+//
+// struktura reprezentująca węża
+//
+
+struct waz {
+    struct punkt cielsko[10000];
+    struct punkt glowa;
+    
+    int dlugosc;    // liczba segmentów węża    
+};
+
+//
+// a oto sam wąż
+//
+
+struct waz w;
+
+int main(int argc, char ** argv)
+{
+    if (init() != 0) {
+        cerr << "Inicjalizacja nie powiodła się." << endl;
+        return -1;
+    }
+ 
+    bool przerysuj = true;
+    bool wyjdz = false;
   
+    //
+    // Tworzymy węża
+    //
+    
     w.glowa.x = 50;
     w.glowa.y = 50;
     
@@ -89,27 +111,38 @@ int main(int argc, char ** argv){
     for (int i = 0; i < w.dlugosc; i++) {
         w.cielsko[i] = w.glowa;
     }
-    
-    while(!doexit)
+
+    //
+    // Event loop - główna pętla programu
+    //
+        
+    while(!wyjdz)
     {
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
 
         if(ev.type == ALLEGRO_EVENT_TIMER) {
-            redraw = true;
+            //
+            // minęła 1/60 (1/FPS) część sekundy
+            //
+            przerysuj = true;
 
-            if (key[KEY_UP]) {
+            if (key[ALLEGRO_KEY_UP]) {
                 w.glowa.y = w.glowa.y - 1;
             }
-            if (key[KEY_RIGHT]) {
+            if (key[ALLEGRO_KEY_RIGHT]) {
                 w.glowa.x = w.glowa.x + 1;
             }
-            if (key[KEY_DOWN]) {
+            if (key[ALLEGRO_KEY_DOWN]) {
                 w.glowa.y = w.glowa.y + 1;
             }
-            if (key[KEY_LEFT]) {
+            if (key[ALLEGRO_KEY_LEFT]) {
                 w.glowa.x = w.glowa.x - 1;
             }
+            
+            //
+            // Poniższy kod jest nieefektywny. Kiedyś nauczymy się to robić lepiej.
+            //
             
             for (int i = 0; i < w.dlugosc - 1; i++) {
                 w.cielsko[i] = w.cielsko[i+1];
@@ -118,52 +151,25 @@ int main(int argc, char ** argv){
             w.cielsko[w.dlugosc - 1] = w.glowa;
             
         } else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-            switch (ev.keyboard.keycode) {
-            case ALLEGRO_KEY_UP:
-                key[KEY_UP] = true;
-                break;
-            case ALLEGRO_KEY_DOWN:
-                key[KEY_DOWN] = true;
-                break;
-            case ALLEGRO_KEY_LEFT:
-                key[KEY_LEFT] = true;
-                break;
-            case ALLEGRO_KEY_RIGHT:
-                key[KEY_RIGHT] = true;
-                break;
-            }
+            key[ev.keyboard.keycode] = true;
         } else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
-            switch(ev.keyboard.keycode) {
-            case ALLEGRO_KEY_UP:
-                key[KEY_UP] = false;
-                break;
-            case ALLEGRO_KEY_DOWN:
-                key[KEY_DOWN] = false;
-                break;
-            case ALLEGRO_KEY_LEFT:
-                key[KEY_LEFT] = false;
-                break;
-            case ALLEGRO_KEY_RIGHT:
-                key[KEY_RIGHT] = false;
-                break;
-            case ALLEGRO_KEY_ESCAPE:
-                doexit = true;
-                break;
+            key[ev.keyboard.keycode] = false;
+            
+            if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+                wyjdz = true;
             }
-         }
+        }
 
-        if(redraw && al_is_event_queue_empty(event_queue)) {
-            redraw = false;
+        if(przerysuj && al_is_event_queue_empty(event_queue)) {
+            przerysuj = false;
 
             al_clear_to_color(al_map_rgb(0,0,0));
      
             for (int i = 0; i < w.dlugosc; i++) {
                 al_draw_filled_rectangle(
-                  w.cielsko[i].x * segment, 
-                  w.cielsko[i].y * segment,
-                  (w.cielsko[i].x+1) * segment - 1,
-                  (w.cielsko[i].y+1) * segment - 1,
-                  al_map_rgb(98,76,54));
+                      w.cielsko[i].x * segment, w.cielsko[i].y * segment,
+                      (w.cielsko[i].x+1) * segment - 1, (w.cielsko[i].y+1) * segment - 1,
+                      al_map_rgb(98,76,54));
             }
              
             al_flip_display();
