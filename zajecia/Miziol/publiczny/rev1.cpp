be755cf4fa04b30e9e2a259aa6c8a05768a61bd6
KM <k.miziolek08@gmail.com>
2013-02-22 17:07:57 +0100

    To moj rev1.cpp

---
+++ zajecia/Miziol/publiczny/rev1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+string rev(string s)
+	{
+	char n;
+	int l=s.length()/2;
+	for(int i=0;i<l;i++)
+		{
+		n=s[(s.length()-i)-1];
+		s[(s.length()-i)-1]=s[i];
+		s[i]=n;
+		}
+
+	return s;
+	}
+
+
+int main()
+{
+string s;
+getline(cin,s);
+cout<<rev(s)<<"\n";
+return 0;
+}
