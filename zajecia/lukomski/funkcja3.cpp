f9e7c41221657f4a571ac979f9ad781664e264e0
lukomski <janek.lukomski98@gmail.com>
2013-02-22 16:21:20 +0100

    funkcja 3

---
+++ zajecia/lukomski/funkcja3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+	int a=0,b=1,c;
+	for(int i=1;i<n;i++){
+	c=a+b;
+	a=b;
+	b=c;
+	}
+	return a;
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<fibonacci(n)<<"\n";
+}
