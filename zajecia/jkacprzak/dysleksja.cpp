a5f88b30f87490e5fe42055e79a3915dd5474ef2
jkacprzak <janek.kacprzak@gmail.com>
2013-02-28 20:29:32 +0100

    Moje rozwiąznie zadania dysleksja

---
+++ zajecia/jkacprzak/dysleksja.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int error = 0;
+	string ajs;
+	string cfaj;
+	getline (cin,ajs);
+	getline (cin,cfaj);
+
+
+	
+		for(int i= 0; i < ajs.length(); i++){
+
+		if(cfaj[i] != ajs[i]){ wynik++; }}
+
+
+		cout << error<<"\n";
+
+
+
+return 0;
+}
