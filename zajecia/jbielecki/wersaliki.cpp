ea0a143227670cf61087293647a25a35ecd1ac22
Janek Bielecki <jan.bielecki@outlook.com>
2013-03-01 15:30:56 +0100

    Dorobilem wciecia w moim rozwiazaniu zadania wersaliki

--- zajecia/jbielecki/wersaliki.cpp
+++ zajecia/jbielecki/wersaliki.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 int main() {
-string s;
-getline(cin, s);
-string male = "aąbcćdeęfghijklłmnńoóprsśtuwyzźż";
-string duze = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ";
-for(int i = 0; i < s.length(); i++){
-int j = 0;
-while(j < male.length() && s[i] != male[j]){
-j++;
-}
-if(s[i] == male[j]){
-cout << duze[j];}
-else {
-cout << s[i];
+	string s;
+	getline(cin, s);
+	
+	string male = "aąbcćdeęfghijklłmnńoóprsśtuwyzźż";
+	string duze = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ";
+	
+	for(int i = 0; i < s.length(); i++){
+	
+	int j = 0;
+	
+	while(j < male.length() && s[i] != male[j]){
+	j++;
+	}
+	if(s[i] == male[j]){
+	cout << duze[j];}
+	else {
+	cout << s[i];
 }
 }
 }
