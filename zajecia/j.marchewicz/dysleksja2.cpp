d387906adbc780cf7d3f4c728e392e66bbc568c4
jmarchewicz <jan.marchewicz@gmail.com>
2013-03-02 17:38:00 +0100

    Teraz są obie wersje dysleksji dla MAIN

--- zajecia/j.marchewicz/dysleksja2.cpp
+++ zajecia/j.marchewicz/dysleksja2.cpp
@@ -4,26 +4,20 @@
 using namespace std;
 int main()
 {
-	string a;
-	string s;
-	for(int i=0; i!=1;)
-	{
-		cout << "Prawidłowa odpowiedź: "<< endl;
-		getline (cin,a);
-		cout << "Odpowiedź pytanego: "<< endl;
-		getline (cin,s);
-		if (s.length() != a.length())
-		{
-			cout<< "Odpowiedź pytanego jest za krótka o " << int (a.length() - s.length()) << " znaków."<< endl << "Wpisz jeszcze raz" << endl;
-		}
-		else i++;
-	}
-		int wynik=0;
-		for (int i=0; i < s.length(); i++)
-		{
-			if(s[i] != a[i]) {wynik++;}
-		}
+	int n;	
+	string r;
+	string t;
 
-	cout<<"Liczba błędów: "<<wynik<< endl;
+	cin >> n;
+	cin >> r;
+	cin >> t;
 
+	int wynik = 0;
+	for(int i= 0; i < n; i++){
+
+	if(t[i] != r[i]){ wynik++; }}
+
+
+	cout << wynik;
 }
+
