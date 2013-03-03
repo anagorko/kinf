484ffbc9dfb92512f6ffc2e6892b7881bc5425e7
Janek <janek@ubuntu.ubuntu-domain>
2013-03-03 21:36:16 +0100

    program wiezyczki last dition

--- zajecia/lukomski/wieze.cpp
+++ zajecia/lukomski/wieze.cpp
@@ -9,12 +9,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n;
-	cin>>n;
-	int t[1000001],ta[n][2];
-	int kon=0,least=1000001,most=0;
+	int n;//liczba wierzyczek
+	cin>>n;//wczytywanie ilosci wierzyczek
+	int t[1000000],ta[n][2];//w tablicy 't' trzymam wszystkie wieze, a w tablicy 'ta' trzymam wspolwrzedne do tablict 't' i wysokosc wiez;
+	int kon=0,least=1000001,most=0;// least to ilosc klockow w najnizszej wiezy, most to ilosc klockow w najwiekszej wiezy
 	//czyszczenie tablic
-	for(int i=0;i<1000001;i++){
+	for(int i=0;i<1000000;i++){
 		t[i]=0;
 		if(i<n){
 		ta[i][0]=0;
@@ -22,6 +22,7 @@ int main(){
 		}
 	}
 	//end of czyszczenie tablic
+	//wczytywanie wiez
 	for(int i=0;i<n;i++){
 		cin>>ta[i][0];
 		ta[i][1]=kon;
@@ -31,31 +32,29 @@ int main(){
 		cin>>t[kon];
 		kon++;
 		}
-	}ta[n][1]=kon;
-	cout<<"rozwiazywanie..."<<endl;
-	//rozwiazywanie
+	}
 	int suma=0;
-	int max=0;
-	for(int a=0;a<most-least;a++){
-		for(int i=0;i<n;i++){
-			if(a<=ta[i][0]){
-				if(t[ta[i+1][1]-a]>max){max=t[ta[i+1][1]-a];cout<<"max =  "<<max<<endl;}
-			}
-		}
-		suma=suma+max;
-		cout<<"suma = "<<suma<<endl;
-		max=0;
+	int sumamax=0;
+	int w=0;//wynik
+	for(int i=0;i<n;i++){///////////////////////
+		for(int a=0;a<least;a++){	  //
+			suma=suma+t[ta[i][1]+a];  //znajduje sume najwieksza dla najmniejszej ilosci klockow
+		}				  //
+		if(suma>sumamax){sumamax=suma;}	  //
+		suma=0;				  //
+	}///////////////////////////////////////////
+	w=sumamax;
+	sumamax=0;
+	cout<<"w na poczatku = "<<w<<endl;
+	for(int i=least;i<most;i++){////////////////////////////////////
+		for(int a=0;a<n;a++){				      //
+			if(i<ta[a][0] && t[ta[a][1]+i]>sumamax){sumamax=t[ta[a][1]+i];}//cout<<"w ="<<sumamax<<endl;}
+		}						      //tutaj sa liczone sumy najwiekszych klockow w "rzedzie"
+		cout<<"w ="<<sumamax<<endl;			      //
+		w=w+sumamax;					      //
+		sumamax=0;					      //
+		////////////////////////////////////////////////////////
 	}
-	cout<<suma<<endl;
-	int suma1=0;
-	for(int i=0;i<n;i++){
-		for(int a=0;a<least;a++){
-			suma1=suma1+t[ta[i][1]+a];
-		}
-		if(suma1>max){max=suma1;}
-	}
-	suma=suma+suma1;
-	cout<<suma<<endl;
-	//i tu reszta kodu, ale nie wiem czemu nie działa to co do tej pory.
+	cout<<w<<endl;
 return 0;
 }
