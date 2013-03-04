d511bf287ed41e2e5c61957825b9adf04f8e6f22
Janek Bielecki <jan.bielecki@outlook.com>
2013-03-04 20:25:40 +0100

    Zadanie inicjalizacja przyklad e

---
+++ zajecia/jbielecki/inicjalizacja/inicjalizacja_e.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int tab[n];
+	long  z = 4;
+	
+	for(long i = 0; i < n; i++)
+	
+	{
+	
+	
+	tab[i] = z;
+	
+	z ++;
+	}
+
+	for(long i = 0; i < n; i++)
+	
+	{
+	
+	cout << "tab[" << i << "] = " << tab[i] << endl;
+	
+	}
+
+}
