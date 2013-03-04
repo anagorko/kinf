1cde013a64eb54986e902cdb758479bc913c1f44
Janek Bielecki <jan.bielecki@outlook.com>
2013-03-04 19:56:54 +0100

    Moje rozwiazanie zadania parzyste przodem

---
+++ zajecia/jbielecki/parzyste_przodem.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tab[20];
+	
+	for(int i = 0; i < 20; i++)
+	
+	{
+
+	cin >> tab[i];
+	
+	}
+
+	for(int i = 0; i < 20; i++) 
+	
+	{
+
+	if((tab[i] % 2) == 0)
+		{
+	
+	cout << tab[i];
+		}
+
+	}
+	for(int i = 0; i < 20; i++) 
+	{
+
+	if((tab[i] % 2) != 0)
+		{
+	
+	cout << " " << tab[i];
+		}
+
+	}
+}
