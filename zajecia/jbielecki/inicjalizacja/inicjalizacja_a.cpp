573bf973ff0b789f59f02c6cb70e442452286fb0
Janek Bielecki <jan.bielecki@outlook.com>
2013-03-04 20:24:54 +0100

    Zadanie inicjalizacja przyklad a

---
+++ zajecia/jbielecki/inicjalizacja/inicjalizacja_a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int tab[n];
+	
+	for(int i = 0; i < n; i++)
+	
+	{
+	
+	tab[i] = i;
+	
+	}
+
+	for(int i = 0; i < n; i++)
+	
+	{
+	
+	cout << "tab[" << i << "] = " << tab[i] << endl;
+	
+	}
+
+}
