0af995cb2ed0eed8b7beb7e50beb501be48b05ec
lukomski <janek.lukomski98@gmail.com>
2013-03-06 21:12:09 +0100

    program przyciski z Kacika Olimpijskiego

--- zajecia/lukomski/przyciski.cpp
+++ zajecia/lukomski/przyciski.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
         int n;
         int m;
-        int max=0;
+        int max=0,min=0;;
         cin>>n>>m;
         int t[n];
         for(int i=0;i<n;i++){t[i]=0;}
@@ -11,15 +11,18 @@ int main(){
                 int p;
                 cin>>p;
                 if(p==n+1){
-                        for(int a=0;a<n;a++){t[a]=max;}
+                        min=max;
                 }else{
-                        t[p-1]++;
+                        if(t[p-1]>min){t[p-1]++;
+			}else{t[p-1]=min+1;}
                         if(t[p-1]>max){max=t[p-1];}
                 }
-        }
+	}
         for(int i=0;i<n;i++){
-        cout<<t[i]<<" ";
-        }
+		if(t[i]>min){
+        	cout<<t[i]<<" ";
+       		}else{cout<<min<<" ";}
+	}
         cout<<endl;
 }
 
