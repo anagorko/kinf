02050b5aa5fa0b56365217e86e44dd2c1fd912c4
lukomski <janek.lukomski98@gmail.com>
2013-03-06 19:20:18 +0100

    program przyciski na 53pkt

--- zajecia/lukomski/przyciski.cpp
+++ zajecia/lukomski/przyciski.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n;
-	int m;
-	cin>>n>>m;
-	int t[n];
-	for(int i=0;i<n;i++){t[i]=0;}
-	for(int i=0;i<m;i++){
-		int p;
-		cin>>p;
-		if(p<=n){t[p-1]++;}
-		if(p==n+1){
-			int max=0;
-			for(int a=0;a<n;a++){
-				if(t[a]>max){max=t[a];}
-			}
-			for(int a=0;a<n;a++){t[a]=max;}
-		}
-	}
-	for(int i=0;i<n;i++){
-	cout<<t[i]<<" ";
-	}
-	cout<<endl;
+        int n;
+        int m;
+        int max=0;
+        cin>>n>>m;
+        int t[n];
+        for(int i=0;i<n;i++){t[i]=0;}
+        for(int i=0;i<m;i++){
+                int p;
+                cin>>p;
+                if(p==n+1){
+                        for(int a=0;a<n;a++){t[a]=max;}
+                }else{
+                        t[p-1]++;
+                        if(t[p-1]>max){max=t[p-1];}
+                }
+        }
+        for(int i=0;i<n;i++){
+        cout<<t[i]<<" ";
+        }
+        cout<<endl;
 }
+
+
+
