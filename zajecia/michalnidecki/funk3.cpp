cf191daa08e436f6a688e0c98fc5a78e46a5ed6f
wiesiek <michal.nidecki@gmail.com>
2013-03-07 20:58:03 +0100

    Funkcje 3

--- zajecia/michalnidecki/funk3.cpp
+++ zajecia/michalnidecki/funk3.cpp
@@ -1,3 +1,4 @@
+
 //
 // Przyjmuje się, że pierwszym i drugim wyrazem ciągu Fibnoacciego jest 1, 1
 // Po poprawieniu usterek usuń ten komentarz.
@@ -7,7 +8,7 @@ using namespace std;
 int fibonacci(int n)
 {
 	int a = 0, b = 1, c;
-	for(int i = 1; i < n; i++){
+	for(int i = 0; i < n; i++){
 		c = a + b;
 		a = b;
 		b = c;
