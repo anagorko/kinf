0b216c7d7f17c31c954c8304582f4db46305393c
jkacprzak <janek.kacprzak@gmail.com>
2013-03-16 11:17:35 +0100

    Moje rozwiązanie ciągu Fibonaccieigo

---
+++ zajecia/jkacprzak/ciag.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main (){
+int had = 0, have = 1,x , suma, parzy ;
+	
+	for (; parzy < 4000000;){ 
+		
+		x = had + have;
+
+		if (x % 2 == 0){
+			parzy += x; 
+		}
+
+		had = have;
+
+		have = x;
+
+		suma += x;
+		
+				
+		
+	}
+
+	cout <<	endl << parzy << endl ;
+
+return 0;
+}
