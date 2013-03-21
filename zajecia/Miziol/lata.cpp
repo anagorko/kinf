6bd0c585d6aeab8720427f523fca4afc7499c97c
Miziol <k.miziolek08@gmail.com>
2013-03-21 21:51:03 +0100

    Moj program lata.cpp

---
+++ zajecia/Miziol/lata.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int r,k,d,e,i,a,b,c;
+cin>>r;
+cin>>k;
+cin>>d;
+a=(k*2);
+b=d-a;
+c=r/2;
+e=b/2-c;
+i=e+r;
+cout<<e<<" "<<i<<"\n";
+return 0;
+}
