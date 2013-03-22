22da75acc96fdbb219ab1dcce27a99dc7194c07b
pnagorko <wuch@gazeta.pl>
2013-03-22 16:48:20 +0100

    Moje rozwiązania ćwiczeń z instrukcji warunkowej

---
+++ zajecia/pnagorko/g4.cpp
@@ -0,0 +1,17 @@
+#include<allegro.h>
+#include<iostream>
+using namespace std ;
+	int main (){
+		int a,s,d,f,c;
+		cin>>a>>s>>d;
+		if(a + s+ d==21){
+			cout<<"tak\n";
+		}else{
+			cout<<"nie\n";
+		}
+
+return 0;	
+}
+		
+		
+
