7fd7dcc4c53c5e756efe2f6bb5de88b59979fe2d
KM <k.miziolek08@gmail.com>
2013-04-05 18:01:05 +0200

    Moja mrówka Lanktona

--- zajecia/Miziol/plansza2d.cpp
+++ zajecia/Miziol/plansza2d.cpp
@@ -17,6 +17,10 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
+int t[1][3];//"0/0"=x;"O/1"=y;"0/1"=kierunek
+t[0][0]=50;
+t[0][1]=50;
+t[0][2]=0;
     if (plansza2d_init(px * 5 + 10, py * 5 + 10) != 0)
     {
         cerr << "Inicjalizacja nie powiodła się." << endl;
@@ -42,8 +46,30 @@ int main(int argc, char ** argv)
             }
         }
 
-        plansza[random() % px][random() % py] = al_map_rgb(random() % 256, random() % 256, random() % 256);
-        
+        //start mrówka
+
+        if(t[0][2]==0){t[0][1]--;}
+        if(t[0][2]==1){t[0][0]--;}
+        if(t[0][2]==2){t[0][1]++;}
+        if(t[0][2]==3){t[0][0]++;}
+
+        if (color_compare(plansza[t[0][0]][t[0][1]],al_map_rgb(0,0,0))) 
+            {
+            t[0][2]++;
+            plansza[t[0][0]][t[0][1]] = al_map_rgb(255,255,255);
+            }
+        else
+            {
+            t[0][2] += 3;
+            plansza[t[0][0]][t[0][1]] = al_map_rgb(0,0,0);
+            }
+
+        t[0][0]=t[0][0]%100;
+        t[0][1]=t[0][1]%100;
+        t[0][2]=t[0][2]%4;
+
+        //koniec mrówka
+       
         plansza2d_rysuj(plansza, 5, 5);
 
         al_flip_display();
