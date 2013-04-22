c3ca1e651cb304dd0e2ab5cc5352766e07c9f9c5
Janek Bielecki <janek@janek-AOD255E.(none)>
2013-04-22 16:11:41 +0200

    Sortowanie babelkowe

--- zajecia/jbielecki/sortowanie swiczenia/booble_sort.cpp
+++ zajecia/jbielecki/sortowanie swiczenia/booble_sort.cpp
@@ -23,37 +23,3 @@ for(int i=0;i<n;i++)
 	cout << endl;
 	   return 0;
 }
-/*
-#include <iostream>
-using namespace std;
-int main(){
-	int n;
-	cin >> n;
-	int data[n];
-	for(int i = 0; i < n; i++) cin >> data[i];
-	int SubArrayEnd = n -1;  
-
-	while (SubArrayEnd > 0)
-	{
-	int nextEnd = 0;
-    
-	for (int j = 0; j < (SubArrayEnd); j++)
-	{
-    if (data[j] > data[j+1])
-	{
-	int temp=data[j]; 
-       data[j]=data[j+1];      
-       data[j+1]=temp;   
-
-
-       nextEnd = j;
-	}
-	else  
-	++j;
-	}
-
-	SubArrayEnd = nextEnd;
-	}
-	for(int i = 0; i < n; i++) cout << data[i] << " ";
-	cout << endl
-}*/
