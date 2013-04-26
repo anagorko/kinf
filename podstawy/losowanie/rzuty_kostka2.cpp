0329036ec3081c836b18d8fee4ff62d9e66cbf35
Andrzej Nagórko <amn@mimuw.edu.pl>
2013-04-26 11:11:25 +0200

    Przykłady pokazujące, jak używać generatora liczb losowych

---
+++ podstawy/losowanie/rzuty_kostka2.cpp
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    srandom(time(NULL));
+    
+    cout << "Wyniki dziesięciokrotnego rzutu kostką: ";
+    for (int i = 0; i < 10; i++) {
+        cout << (random() % 6 + 1) << " ";
+    }
+    cout << endl;
+    return 0;
+}
