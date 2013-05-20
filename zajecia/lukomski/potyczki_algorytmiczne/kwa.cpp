8d6eb6a8a9d839929d9608ec39c3e5245c514799
lukomski <janek.lukomski98@gmail.com>
2013-05-20 21:36:05 +0200

    zadanie kwadrat z potyczek algorytmicznych

--- zajecia/lukomski/potyczki_algorytmiczne/kwa.cpp
+++ zajecia/lukomski/potyczki_algorytmiczne/kwa.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-//#include<cmath>
+#include<cmath>
 using namespace std;
 int main(){
 	int n;
@@ -7,7 +7,7 @@ int main(){
 	cin>>n;
 	long long int m=2*n;
 	while(true){
-		if(sqrt(m)-(int)sqrt(m)==0){
+		if(sqrtl(m)-(int)sqrtl(m)==0){
 			cout<<m<<endl;return 0;
 		}
 	m=m+n;
