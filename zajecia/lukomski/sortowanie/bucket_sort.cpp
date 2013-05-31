6a201143976eb239f93901b2f1c7f108d3993164
lukomski <janek.lukomski98@gmail.com>
2013-05-31 16:31:04 +0200

    sotowanie kubelkowe

--- zajecia/lukomski/sortowanie/bucket_sort.cpp
+++ zajecia/lukomski/sortowanie/bucket_sort.cpp
@@ -4,11 +4,22 @@ int main(){
 	int n;
 	cin>>n;
 	int t[n];
-	max=0;
+	int max=0;
 	for(int i=0;i<n;i++){
 		cin>>t[i];
 		if(t[i]>max){max=t[i];}
 	}
 	int ta[max];
 	for(int i=0;i<n;i++){
-		
+		ta[i]=0;
+	}
+	for(int i=0;i<n;i++){
+		ta[t[i]]++;
+	}
+	for(int i=0;i<=max;i++){
+		for(int a=0;a<ta[i];a++){
+			cout<<i<<" ";
+		}
+	}
+	return 0;
+}
