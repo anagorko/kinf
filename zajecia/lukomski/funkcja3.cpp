b2479e3f66b86ad9d8faa920a36f35b72fd98222
lukomski <janek.lukomski98@gmail.com>
2013-06-03 21:15:23 +0200

    poprawiona funkcja3

--- zajecia/lukomski/funkcja3.cpp
+++ zajecia/lukomski/funkcja3.cpp
@@ -1,11 +1,7 @@
-//
-// Przyjmuje się, że pierwszym i drugim wyrazem ciągu Fibnoacciego jest 1, 1
-// Po poprawieniu usterek usuń ten komentarz.
-//
 #include<iostream>
 using namespace std;
 int fibonacci(int n){
-	int a=0,b=1,c;
+	int a=1,b=1,c;
 	for(int i=1;i<n;i++){
 	c=a+b;
 	a=b;
