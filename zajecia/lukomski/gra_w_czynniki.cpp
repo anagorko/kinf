67920347c6053d5c6e31fae0d576f8afb8908cc7
lukomski <janek.lukomski98@gmail.com>
2013-06-05 23:54:55 +0200

    gra w czynniki nadal na 30 pkt

--- zajecia/lukomski/gra_w_czynniki.cpp
+++ zajecia/lukomski/gra_w_czynniki.cpp
@@ -10,14 +10,37 @@ int main(){
 		if(t[i]>max){max=t[i];}
 	}
 	int ta[max];
-	ta[0]=0;
-	ta[1]=0;
-	for(int i=2;i<max;i++){
-		ta[i]=0;
-		for(int a=2;a<=(i+1)/2;a++){
-			if((i+1)%a==0 && ta[i-a]==0){ta[i]=1;break;}
+	bool czypier[max];
+	int pier[max];
+	int kon=0;
+	for(int i=0;i<max/2;i++){//czyszczenie czypier(czy pierwsza);
+		czypier[i]=true;
+	}
+	for(int i=2;i<max;i++){//sito arystotelesa
+		if(czypier[i]=true){
+			pier[kon]=i;
+			kon++;
+			for(int a=i;a<max;a=a+i){
+				czypier[a]=false;
+			}
 		}
 	}
+//	cout<<"wypisywanie liczb pierwszych : ";
+//	for(int i=0;i<kon;i++){//wypisywanie liczb pierwszych
+//		cout<<pier[i]<<" ";
+//	}
+//	cout<<endl;
+	///
+	for(int i=0;i<max;i++){//rozwiazywanie tablicy
+		ta[i]=0;
+		for(int a=0;a<kon && pier[a]<i+1;a++){
+			if(ta[i-pier[a]]==0 && (i+1)%pier[a]==0){ta[i]=1;break;}
+		}
+	}
+//	cout<<"tablica : ";
+//	for(int i=0;i<max;i++){
+//		cout<<ta[i]<<" ";
+//	}
 	for(int i=0;i<n;i++){
 		if(ta[t[i]-1]==1){cout<<"MAGDA\n";
 		}else{
