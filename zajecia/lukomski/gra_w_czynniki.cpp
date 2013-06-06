f70148fd4af601b2af33512a4591628342bf192e
lukomski <janek.lukomski98@gmail.com>
2013-06-06 16:58:24 +0200

    gra w czyniki.cpp 100pkt

--- zajecia/lukomski/gra_w_czynniki.cpp
+++ zajecia/lukomski/gra_w_czynniki.cpp
@@ -13,11 +13,11 @@ int main(){
 	bool czypier[max];
 	int pier[max];
 	int kon=0;
-	for(int i=0;i<max/2;i++){//czyszczenie czypier(czy pierwsza);
+	for(int i=0;i<max;i++){//czyszczenie czypier(czy pierwsza);
 		czypier[i]=true;
 	}
 	for(int i=2;i<max;i++){//sito arystotelesa
-		if(czypier[i]=true){
+		if(czypier[i]==true){
 			pier[kon]=i;
 			kon++;
 			for(int a=i;a<max;a=a+i){
@@ -25,22 +25,12 @@ int main(){
 			}
 		}
 	}
-//	cout<<"wypisywanie liczb pierwszych : ";
-//	for(int i=0;i<kon;i++){//wypisywanie liczb pierwszych
-//		cout<<pier[i]<<" ";
-//	}
-//	cout<<endl;
-	///
 	for(int i=0;i<max;i++){//rozwiazywanie tablicy
 		ta[i]=0;
 		for(int a=0;a<kon && pier[a]<i+1;a++){
 			if(ta[i-pier[a]]==0 && (i+1)%pier[a]==0){ta[i]=1;break;}
 		}
 	}
-//	cout<<"tablica : ";
-//	for(int i=0;i<max;i++){
-//		cout<<ta[i]<<" ";
-//	}
 	for(int i=0;i<n;i++){
 		if(ta[t[i]-1]==1){cout<<"MAGDA\n";
 		}else{
