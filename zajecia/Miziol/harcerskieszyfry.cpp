f2b42724a88db38415b3a9f71ef59c089de93f61
Miziol <k.miziolek08@gmail.com>
2013-06-10 22:03:50 +0200

    Aktualizacja harcerski szyfr.cpp

--- zajecia/Miziol/harcerskieszyfry.cpp
+++ zajecia/Miziol/harcerskieszyfry.cpp
@@ -7,7 +7,7 @@ int r;
 string s;
 cout<<"Wybierz szyfr:\n 1. Morse'a\n 2. GA-DE-RY-PO-LU-KI\n 3. PO-LI-TY-KA-RE-NU\n 4. KONIEC\n";
 cin>>r;
-if(r==1)
+if(r==1)//Morse'a
 	{
 	cout<<"Proszę wpisać tekst.\n Program ten przetwarza tylko alfabet angielski oraz cyfry w pełnym zapisie\n";
 	getline(cin, s);
@@ -52,18 +52,47 @@ if(r==1)
 		if(s[i]==' '&&s[i+1]==' ')cout<<" ";
 		if(s[i]==' '&&s[i-1]!=' ')cout<<"/";
 		}
+	cout<<"/\n";
 	}
 
-if(r==2)
+if(r==2)//Gaderypoluki
+	{
+	cout<<"Proszę wpisać tekst.\n Program ten przetwarza tylko alfabet angielski oraz cyfry w pełnym zapisie\n";
+	getline(cin, s);
+	for(int i=0;i<s.size();i++)
+		{
+		if(s[i]=='A')s[i]=='G';
+		if(s[i]=='a')s[i]=='g';
+		if(s[i]=='D')s[i]=='E';
+		if(s[i]=='d')s[i]=='e';
+		if(s[i]=='E')s[i]=='D';
+		if(s[i]=='e')s[i]=='d';
+		if(s[i]=='G')s[i]=='A';
+		if(s[i]=='g')s[i]=='a';
+		if(s[i]=='I')s[i]=='K';
+		if(s[i]=='i')s[i]=='k';
+		if(s[i]=='K')s[i]=='I';
+		if(s[i]=='k')s[i]=='i';
+		if(s[i]=='L')s[i]=='U';
+		if(s[i]=='l')s[i]=='u';
+		if(s[i]=='O')s[i]=='P';
+		if(s[i]=='o')s[i]=='p';
+		if(s[i]=='P')s[i]=='O';
+		if(s[i]=='p')s[i]=='o';
+		if(s[i]=='R')s[i]=='Y';
+		if(s[i]=='r')s[i]=='y';
+		if(s[i]=='U')s[i]=='L';
+		if(s[i]=='u')s[i]=='l';
+		if(s[i]=='Y')s[i]=='R';
+		if(s[i]=='y')s[i]=='r';
+		cout<<s[i];
+	}
+
+if(r==3)//Politykarenu
 	{
 	
 	}
-
-if(r==3)
-	{
-	
-	}
-cout<<"/\n";
+cout<<"\n";
 
 return 0;
 }
