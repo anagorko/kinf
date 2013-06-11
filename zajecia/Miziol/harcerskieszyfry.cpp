66ffa3a6f1ab297e41426c28be124fdef6dfd4c9
KM <k.miziolek08@gmail.com>
2013-06-11 15:06:06 +0200

    Aktualizacja szyfr harcerski.cpp

--- zajecia/Miziol/harcerskieszyfry.cpp
+++ zajecia/Miziol/harcerskieszyfry.cpp
@@ -7,6 +7,7 @@ int r;
 string s;
 cout<<"Wybierz szyfr:\n 1. Morse'a\n 2. GA-DE-RY-PO-LU-KI\n 3. PO-LI-TY-KA-RE-NU\n 4. KONIEC\n";
 cin>>r;
+cin.ignore(1000000, '\n');
 if(r==1)//Morse'a
 	{
 	cout<<"Proszę wpisać tekst.\n Program ten przetwarza tylko alfabet angielski oraz cyfry w pełnym zapisie\n";
@@ -49,43 +50,43 @@ if(r==1)//Morse'a
 		if(s[i]=='8')cout<<"---../";
 		if(s[i]=='9')cout<<"----./";
 		if(s[i]=='0')cout<<"-----/";
-		if(s[i]==' '&&s[i+1]==' ')cout<<" ";
-		if(s[i]==' '&&s[i-1]!=' ')cout<<"/";
+		if(s[i]==' ')cout<<"/";
 		}
 	cout<<"/\n";
 	}
 
 if(r==2)//Gaderypoluki
 	{
-	cout<<"Proszę wpisać tekst.\n Program ten przetwarza tylko alfabet angielski oraz cyfry w pełnym zapisie\n";
+	cout<<"Proszę wpisać tekst.\n";
 	getline(cin, s);
 	for(int i=0;i<s.size();i++)
 		{
-		if(s[i]=='A')s[i]=='G';
-		if(s[i]=='a')s[i]=='g';
-		if(s[i]=='D')s[i]=='E';
-		if(s[i]=='d')s[i]=='e';
-		if(s[i]=='E')s[i]=='D';
-		if(s[i]=='e')s[i]=='d';
-		if(s[i]=='G')s[i]=='A';
-		if(s[i]=='g')s[i]=='a';
-		if(s[i]=='I')s[i]=='K';
-		if(s[i]=='i')s[i]=='k';
-		if(s[i]=='K')s[i]=='I';
-		if(s[i]=='k')s[i]=='i';
-		if(s[i]=='L')s[i]=='U';
-		if(s[i]=='l')s[i]=='u';
-		if(s[i]=='O')s[i]=='P';
-		if(s[i]=='o')s[i]=='p';
-		if(s[i]=='P')s[i]=='O';
-		if(s[i]=='p')s[i]=='o';
-		if(s[i]=='R')s[i]=='Y';
-		if(s[i]=='r')s[i]=='y';
-		if(s[i]=='U')s[i]=='L';
-		if(s[i]=='u')s[i]=='l';
-		if(s[i]=='Y')s[i]=='R';
-		if(s[i]=='y')s[i]=='r';
-		cout<<s[i];
+		if(s[i]=='A'){s[i]='G';continue;}
+		if(s[i]=='a'){s[i]='g';continue;}
+		if(s[i]=='D'){s[i]='E';continue;}
+		if(s[i]=='d'){s[i]='e';continue;}
+		if(s[i]=='E'){s[i]='D';continue;}
+		if(s[i]=='e'){s[i]='d';continue;}
+		if(s[i]=='G'){s[i]='A';continue;}
+		if(s[i]=='g'){s[i]='a';continue;}
+		if(s[i]=='I'){s[i]='K';continue;}
+		if(s[i]=='i'){s[i]='k';continue;}
+		if(s[i]=='K'){s[i]='I';continue;}
+		if(s[i]=='k'){s[i]='i';continue;}
+		if(s[i]=='L'){s[i]='U';continue;}
+		if(s[i]=='l'){s[i]='u';continue;}
+		if(s[i]=='O'){s[i]='P';continue;}
+		if(s[i]=='o'){s[i]='p';continue;}
+		if(s[i]=='P'){s[i]='O';continue;}
+		if(s[i]=='p'){s[i]='o';continue;}
+		if(s[i]=='R'){s[i]='Y';continue;}
+		if(s[i]=='r'){s[i]='y';continue;}
+		if(s[i]=='U'){s[i]='L';continue;}
+		if(s[i]=='u'){s[i]='l';continue;}
+		if(s[i]=='Y'){s[i]='R';continue;}
+		if(s[i]=='y'){s[i]='r';continue;}
+		}
+	cout<<s;
 	}
 
 if(r==3)//Politykarenu
