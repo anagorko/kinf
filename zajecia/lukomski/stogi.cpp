d1ee618037aaaeb9eaa0b20d1faafa63ab2e2ff8
lukomski <janek.lukomski98@gmail.com>
2013-06-24 23:29:14 +0200

    poprawka do stogi.cpp

--- zajecia/lukomski/stogi.cpp
+++ zajecia/lukomski/stogi.cpp
@@ -14,20 +14,27 @@ int main(){
 			x=x/2;
 		}
 	}
-	while(n>0){
-		cout<<kop[1]<<" ";
+	///////////////////////////
+	cout<<"wypisuje kopiec:     \n";
+	for(int i=1;i<=n;i++){
+		cout<<kop[i]<<" ";
+	}
+	cout<<"\nkoniec kopca.\n";
+	///////////////////////////
+	while(n>=1){
+		cout<<kop[1]<<" "<<endl;
 		kop[1]=kop[n];
 		n--;
 		int x=1;
-		while(true){
+		while(n>0){
 			int y=0;
-			if(kop[x]>kop[x*2] && kop[x]>kop[x*2+1]){
+			if((n<=x*2+1) && (kop[x]>kop[x*2] && kop[x]>kop[x*2+1])){
 				if(kop[x*2]>kop[x*2+1]){
-					y=x*2;
-				}else{
 					y=x*2+1;
+				}else{
+					y=x*2;
 				}
-			}else if(kop[x]>kop[x*2] || kop[x]>kop[x*2+1]){
+			}else if((n<=x*2) &&  (kop[x]>kop[x*2] || kop[x]>kop[x*2+1])){
 				if(kop[x]>kop[x*2]){
 					y=x*2;
 				}else{
