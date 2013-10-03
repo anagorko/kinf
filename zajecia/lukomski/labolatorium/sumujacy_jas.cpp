15a104ceaecf9b0b230de9e80ab04fc6a59b0198
lukomski <janek.lukomski98@gmail.com>
2013-10-03 23:29:04 +0200

    sumujacey jas - 20pkt

--- zajecia/lukomski/labolatorium/sumujacy_jas.cpp
+++ zajecia/lukomski/labolatorium/sumujacy_jas.cpp
@@ -3,139 +3,186 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	string a[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-	int przec[n];
+	string s[n];
 	bool dodatni[n];
 	int dodatnie=0;
-	bool wiekszypierwszy;
-	for(int i=0;i<n;i++){//szukanie przecinka
-		if(a[i][0]=='+'){dodatni[i]=true;dodatnie++;}
-		if(a[i][0]=='-'){dodatni[i]=false;}
-		if(a[i][0]!='+' && a[i][0]!='-'){dodatni[i]=true;dodatnie++;}
+	int przec[n];
+	int maxright=100;
+	for(int i=0;i<n;i++){
+		cin>>s[i];
+		if(s[i][0]=='-'){dodatni[i]=false;}
+		if(s[i][0]=='+'){dodatni[i]=true;dodatnie++;}
+		if(s[i][0]!='-' && s[i][0]!='+'){dodatni[i]=true;dodatnie++;}
 		przec[i]=-1;
-		for(int q=0;q<a[i].length();q++){
-			if(a[i][q]==',' || a[i][q]=='.'){
-				przec[i]=q;
-				cout<<"przecinek nr "<<i<<" jest na miejscu "<<q<<" z "<<a[i].length()<<" miesjsc"<<endl;
-				break;
-			}
+		for(int q=0;q<s[i].length();q++){
+                        if(s[i][q]==',' || s[i][q]=='.'){
+                                przec[i]=q;
+//                                cout<<"przecinek nr "<<i<<" jest na miejscu ";
+                                break;
+                        }
 		}
+
 	}
-	int asdf[dodatnie][200];
-	int asdfg[n-dodatnie][200];
+	int t[dodatnie][200];
+	int ta[n-dodatnie][200];
 	for(int i=0;i<200;i++){
-		for(int q=0;q<dodatnie;q++){
-			asdf[q][i]=0;
-		for(int q=0;q<n-dodatnie;q++){
-			asdfg[q][i]=0;
-		}	
-	}
+		for(int a=0;a<n;a++){
+			if(a<dodatnie || a==0){t[a][i]=0;}
+			if(a<n-dodatnie || a==0){ta[a][i]=0;}
 		}
+	}
+	int ujemnaliczba=0;
+	int dodatnialiczba=0;
+//	cout<<"dosedlem"<<endl;
 	for(int i=0;i<n;i++){
 		if(dodatni[i]){
 			if(przec[i]==-1){
-				for(int q=a[i].length()-1;q>=0;q--){
-					asdf[i][100-q]=(int)a[i][a[i].length()-1-q]-'0';
+                                for(int q=s[i].length()-1;q>=0;q--){
+					if(s[i][s[i].length()-1-q]=='+' || s[i][s[i].length()-1-q]=='-'){continue;}
+                                	        t[dodatnialiczba][100-q]=(int)s[i][s[i].length()-1-q]-'0';
 				}
-			}else{
-				for(int q=przec[i]-1;q>=0;q--){
-				asdf[i][100-q]=(int)a[i][przec[i]-1-q]-'0';
-				}
-				for(int q=1;q<a[i].length()-przec[i];q++){
-					asdf[i][100+q]=(int)a[i][przec[i]+q]-'0';
-				}
-			}
-		}else{
-			if(przec[i]==-1){
-				for(int q=a[i].length()-1;q>=0;q--){
-                     			asdfg[i][100-q]=(int)a[i][a[i].length()-1-q]-'0';
-                      	}
-
-			}else{
-				for(int q=przec[i]-1;q>=0;q--){
-              	        	asdfg[i][100-q]=(int)a[i][przec[i]-1-q]-'0';
-               	        }
-              	        for(int q=1;q<a[i].length()-przec[i];q++){
-                	        	asdfg[i][100+q]=(int)a[i][przec[i]+q]-'0';
-              	        }
-			}
-		}
+                        }else{
+                                for(int q=przec[i]-1;q>=0;q--){
+					if(s[i][przec[i]-1-q]=='+' || s[i][przec[i]-1-q]=='-'){continue;}
+                                		t[dodatnialiczba][100-q]=(int)s[i][przec[i]-1-q]-'0';
+                                }
+                                for(int q=1;q<s[i].length()-przec[i];q++){
+					if(s[i][przec[i]+q]=='+' || s[i][przec[i]+q]=='-'){continue;}
+                                 	       t[dodatnialiczba][100+q]=(int)s[i][przec[i]+q]-'0';
+                                }
+	               	}
+			dodatnialiczba++;
+		 }else{
+                        if(przec[i]==-1){
+                                for(int q=s[i].length()-1;q>=0;q--){
+					if(s[i][s[i].length()-1-q]=='+' || s[i][s[i].length()-1-q]=='-'){continue;}
+                                        ta[ujemnaliczba][100-q]=(int)s[i][s[i].length()-1-q]-'0';
+                      		}
+	 		}else{
+                                for(int q=przec[i]-1;q>=0;q--){
+					if(s[i][przec[i]-1-q]=='+' || s[i][przec[i]-1-q]=='-'){continue;}
+                                	ta[ujemnaliczba][100-q]=(int)s[i][przec[i]-1-q]-'0';
+                        	}
+                        	for(int q=1;q<s[i].length()-przec[i];q++){
+					if(s[i][przec[i]+q]=='+' || s[i][przec[i]+q]=='-'){continue;}
+                                        ta[ujemnaliczba][100+q]=(int)s[i][przec[i]+q]-'0';
+                        	}
+                        }
+			ujemnaliczba++;
+                }
 	}
-	for(int i=0;i<n-dodatnie;i++){
-		cout<<i<<"to sie rowna:\n";
-		for(int q=0;q<200;q++){
-			cout<<asdfg[i][q]<<" ";
-			if(q%50==0 && q!=0){cout<<endl;}
-		}
-		cout<<endl;
-	}
-	cout<<endl;
-		for(int i=199;i>=0;i--){
+//	cout<<"jestye"<<endl;
+//	for(int i=0;i<dodatnie;i++){
+//		for(int a=0;a<200;a++){
+//			cout<<t[i][a];
+//		}
+//		cout<<endl;
+//	}
+//	cout<<"ujemne"<<endl;
+//	for(int i=0;i<n-dodatnie;i++){
+//		for(int a=0;a<200;a++){
+//			cout<<ta[i][a];
+//		}
+//		cout<<endl;
+//	}
+	//sumowanie
+	for(int i=199;i>0;i--){
+		if(dodatnie>0){
 			int suma=0;
-			for(int q=0;q<dodatnie;q++){
-				suma=suma+asdf[q][i];
+			for(int a=0;a<dodatnie;a++){
+				suma=suma+t[a][i];
 			}
-			if(suma>10){
-				asdf[dodatnie-1][i]=suma%10;
-				suma=(suma-(suma%10))/10;
-			}else{
-				asdf[dodatnie-1][i]=suma;
-				suma=0;
+			if(suma>=10){
+				t[0][i-1]=(suma-(suma%10))/10;
+				suma=suma%10;
 			}
+			t[0][i]=suma;
 		}
-		int suma=0;
-		for(int i=199;i>=0;i--){
-			for(int q=0;q<n-dodatnie;q++){
-				suma=suma+asdfg[q][i];
+		if(n-dodatnie>0){
+			int suma=0;
+			for(int a=0;a<n-dodatnie;a++){
+				suma=suma+ta[a][i];
 			}
-			if(suma>10){
-				asdfg[n-dodatnie-1][i]=suma%10;
-				suma=(suma-(suma%10))/10;
-			}else{
-				asdfg[n-dodatnie-1][i]=suma;
-				suma=0;
+			if(suma>=10){
+				ta[0][i-1]=(suma-(suma%10))/10;
+				suma=suma%10;
 			}
+			ta[0][i]=suma;
 		}
+	}
+//	cout<<"zsumowane:\n";
+//	for(int i=0;i<200;i++){
+//		cout<<t[0][i];
+//	}cout<<"\nujemne:\n";
+//	for(int i=0;i<200;i++){
+//		cout<<ta[0][i];
+//	}
+//	cout<<"\nskonczylem sumowanie"<<endl;
+	//sprawdzanie ktore jest wieksze
+	bool wiekszypierwszy=false;
+	if(dodatnie>0 && n-dodatnie>0){
 		for(int i=0;i<200;i++){
-			if(asdf[dodatnie-1][i]>asdfg[n-dodatnie-1][i]){wiekszypierwszy=true;break;}
-			if(asdf[dodatnie-1][i]<asdfg[n-dodatnie-1][i]){wiekszypierwszy=true;break;}
-			if(i==199){cout<<"0"<<endl;return 0;}
+	                        if(t[0][i]>ta[0][i]){wiekszypierwszy=true;break;}
+	                        if(t[0][i]<ta[0][i]){wiekszypierwszy=false;break;}
+	                        if(i==199){cout<<"0\n";return 0;}
 		}
+	}else if(dodatnie>0){wiekszypierwszy=true;}
+	//odejmowanie wiekszego od mniejszego
+	if(dodatnie>0 && n-dodatnie>0){
 		if(wiekszypierwszy){
 			int suma=0;
-			for(int i=199;i>=0;i--){
-				if(asdf[dodatnie-1][i]>asdfg[n-dodatnie-1][i]){
-					asdf[dodatnie-1][i]=asdf[dodatnie-1][i]-asdfg[n-dodatnie-1][i];
-				}else{
-					asdf[dodatnie-1][i-1]--;
-					asdf[dodatnie-1][i]=asdf[dodatnie-1][i]+10;
-					asdf[dodatnie-1][i]=asdf[dodatnie-1][i]-asdfg[n-dodatnie-1][i];
-				}
-			}
-		}else{
-		  	int suma=0;
-                        for(int i=199;i>=0;i--){ 
-                                if(asdfg[n-dodatnie-1][i]>asdf[dodatnie-1][i]){
-                                        asdf[dodatnie-1][i]=asdfg[n-dodatnie-1][i]-asdf[dodatnie-1][i];
-                                }else{
-                                        asdfg[n-dodatnie-1][i-1]--;
-                                        asdf[dodatnie-1][i]=asdfg[n-dodatnie-1][i]+10;
-                                        asdf[dodatnie-1][i]=asdfg[n-dodatnie-1][i]-asdf[dodatnie-1][i];
-                                }
-                        }
-		}
-		bool zera;
-		for(int i=0;i<200;i++){
-			if(asdf[dodatnie-1][i]!=0 && zera){zera=false;}
-			if(zera){continue;}
-			cout<<asdf[dodatnie-1][i];
-		}
-		cout<<endl;
-	return 0;
+	                for(int i=199;i>=0;i--){
+	         	      	if(t[0][i]-ta[0][i]>=0){
+	                        	t[0][i]=t[0][i]-ta[0][i];
+	                       	}else{
+	                	       	t[0][i-1]--;
+	                               	t[0][i]=t[0][i]+10;
+	                               	t[0][i]=t[0][i]-ta[0][i];
+	                      	}
+		               	}
+       		}else{
+      		      	int suma=0;
+       		       	for(int i=199;i>=0;i--){ 
+        			if(ta[0][i]-t[0][i]>=0){
+                	        	t[0][i]=ta[0][i]-t[0][i];
+       	              		}else{
+ 	   	                   	ta[0][i-1]--;
+ 	                         	t[0][i]=ta[0][i]+10;
+ 		       	          	t[0][i]=ta[0][i]-t[0][i];
+                	        }
+       	         	}
+   		}
+	}else if(!wiekszypierwszy){
+		cout<<"-";
+	        int licznik=0;
+	        while(ta[0][licznik]==0){
+	                licznik++;
+	        }
+	        for(int i=licznik;i<=100;i++){
+	                cout<<ta[0][i];
+	        }
+	        licznik=199;
+	        while(t[0][licznik]==0){
+	                licznik--;
+	        }
+       		for(int i=101;i<licznik;i++){
+       	        	cout<<ta[0][i];
+       	 	}cout<<endl;
+	}
+	//wynik
+	if(!wiekszypierwszy){cout<<"-";}
+	int licznik=0;
+	while(t[0][licznik]==0){
+		licznik++;
+	}
+	for(int i=licznik;i<=100;i++){
+		cout<<t[0][i];
+	}
+	licznik=199;
+	while(t[0][licznik]==0){
+		licznik--;
+	}
+	for(int i=101;i<licznik;i++){
+		cout<<t[0][i];
+	}cout<<endl;
 }
-
-
-
