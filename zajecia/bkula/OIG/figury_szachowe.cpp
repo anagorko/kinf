f58f56a1263347e625416450c4e5e77a0012a593
bkula <bernard.kula@gmail.com>
2013-10-05 21:50:24 +0200

    Zadanie 'Figury szachowe' z labolatroium OIG.

---
+++ zajecia/bkula/OIG/figury_szachowe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int K, Q, W, G, S, P;
+
+    cin >> K >> Q >> W >> G >> S >> P;
+    cout<<1-K<<" "<<1-Q<<" "<<2-W<<" "<< 2-G<<" "<<2-S<<" "<<8-P;
+
+    return 0;
+}
+
