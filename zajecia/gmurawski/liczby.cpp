1ba5b3206ee4cb6dc88bd457ae50b9a3010c9c87
lukomski <gabimurawski@wp.pl>
2013-10-18 16:30:03 +0200

    Rozwiązanie zadania Read/Write

---
+++ zajecia/gmurawski/liczby.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main()
+
+{
+int a, b, c;
+cout << " Wypisz trzy liczby.";
+cin >> a;
+cin >> b;
+cin >> c;
+cout << " Liczby to : " << a << " "<< b << " " << c;
+cout << " Odwrócenie tych liczb to : " << c << " "<< b << " "<< a;
+ 
+return 0 ;
+}
