2ce4f3542b96dddef406c07209522f8dd59d5f30
skibin <skibius00@gmail.com>
2013-10-25 16:49:48 +0200

    wedlóg wstosdu

---
+++ zajecia/skibin/wiekszza.cpp
@@ -0,0 +1,10 @@
+//wieksza.cpp
+#include<iostream>
+using namespace std;
+int main()
+{
+float a, b;
+cout<<"podaj wzrost twój i kolegi";
+cin>>a>>b;
+if(a>b){cout<<a<<"'"<<b;}
+else{cout<<b<<"'"<<a;}}
