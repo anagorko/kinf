1888c52a094c49fcdc1eab2dd997dfb39b16999c
bkula <bernard.kula@gmail.com>
2013-11-14 00:48:44 +0100

    Liczba cyfr z labolatorium OIG

--- zajecia/bkula/OIG/Liczba_cyfr.cpp
+++ zajecia/bkula/OIG/Liczba_cyfr.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
 
-    int a, w = 0;
-    cin >> a;
-    for (int i = 10; i < a; i *= 10) w++;
-    cout << w + 1;
-}
+    string txt;
+    cin >> txt;
+    cout << txt.length();
 
+    return 0;
+}
