ff1ba258d59c1163c6b1e834c891c9d77cf5feee
pjpawel <pawel.jan.podgorski@gmail.com>
2014-02-07 17:04:22 +0100

    Rozwiazania dwoch zadan z laboratorium

---
+++ zajecia/pjpawel/dwiekolejnewieksze.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int k, q, w, g, s, p;
+    if(k=1){
+    cin >> "0"} 
+    if(k<1){
+    cin >> "1"}   
+    if(k>1){
+    cin >> k-1}   
+
+
+
+
+
+
+
+
+
+
+  
+}
