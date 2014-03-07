efc73c75cab9fdf9308f22e922c2b6e94132360e
pnagorko <pawel.jan.podgorski@gmail.com>
2014-03-07 16:59:54 +0100

    NIe zdazylem

---
+++ zajecia/j.marchewicz/wężyk.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{ 
+int n;
+cin>> n;
+
+for (int i=0; i < n; i++) 
+	{
+		if ((i+1) % 2 == 1)
+		{
+			for (int e=1; e <=n; e++) 
+			{ 
+				tab [e-1] [n]
