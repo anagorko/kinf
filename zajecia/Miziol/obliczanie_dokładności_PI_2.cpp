dac0ff71b097e82329853ac433f05f38be60fd4a
Miziol <k.miziolek08@gmail.com>
2014-03-15 12:24:56 +0100

    zadanie z wczorajszego kółka

--- zajecia/Miziol/obliczanie_dokładności_PI_2.cpp
+++ zajecia/Miziol/obliczanie_dokładności_PI_2.cpp
@@ -11,14 +11,14 @@ srandom(time(NULL)+getpid());
 float r=0,x=0,y=0,n=0;
 int w=0;
 cin>>n;
-for( int i=0;i<n;i++)
+for(int i=0;i<n;i++)
 	{
 	x=(random()%10000001)/10000000.0;
 	y=(random()%10000001)/10000000.0;
 	r=sqrt(((x-0.5)*(x-0.5))+(y-0.5)*(y-0.5));
 	if(r<0.5){w++;}
 	}
-cout<<w/n<<"\n";
+cout<<4*(w/n)<<"\n";
 
 return 0;
 }
