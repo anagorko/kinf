5098f6f16b647edfa057625e2f8aec9c156dedf3
Jan Marchewicz <jan.marchewicz@gmail.com>
2014-04-22 17:44:42 +0200

    ostry, prosty, rozwarty

---
+++ zajecia/j.marchewicz/ostry_prosty_rozwarty.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int a;
+cin >> a;
+
+	if ( a < 90)
+	{
+		cout << "OSTRY\n";
+	}
+	else if ( a == 90)
+	{
+		cout << "PROSTY\n";
+	}
+	else 
+	{
+		cout << "ROZWARTY\n";
+	}
+}
