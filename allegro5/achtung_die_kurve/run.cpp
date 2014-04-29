c1332aa7659f061aa22974c14b68dcd57e4f49a7
Andrzej Nagórko <antek.fiedoruk@gmail.com>
2014-04-29 10:07:09 +0200

    Inny sposób oczekiwania na połączenie

--- allegro5/achtung_die_kurve/run.cpp
+++ allegro5/achtung_die_kurve/run.cpp
@@ -6,8 +6,11 @@ int run_server(){
 	if (!connect_to_server("ubuntu")) {
         cout << "Połączenie nie powiodło się. Odpalam server\n";
         	system ("./server&");
-        	usleep(4000);
-        	connect_to_server("ubuntu"); 
+        	int licznik = 0;
+        	while (!connect_to_server("ubuntu") && licznik < 20) {
+        	    licznik++;
+            	usleep(500);
+        	}
         	return 1;
   	}else{
   		return 0;
