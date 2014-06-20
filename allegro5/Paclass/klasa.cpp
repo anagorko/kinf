097569757e9a39c66e4bfddc46119c617e718205
janek.kacprzak@gmail.com <janek.kacprzak@gmail.com>
2014-06-20 19:42:52 +0200

    Próba przerobienia pacmana na programowanie obiektowe (nie działa)

---
+++ allegro5/Paclass/klasa.cpp
@@ -0,0 +1,240 @@
+//
+// Szablon gry w Allegro 5. (C) Kółko Informatyczne Szkoły Żagle
+//
+
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+using namespace std;
+
+//
+// Konfiguracja gry
+//
+
+const int screen_w = 1000;   // szerokość ekranu (screen width)
+const int screen_h = 562;   // wysokość ekranu (screen height)
+
+/****************************************
+ * Tu rozpoczyna się istotna część kodu *
+ ****************************************/
+ 
+//
+// Struktury danych
+//
+bool key[ALLEGRO_KEY_MAX];  // wciśnięte klawisze
+//
+// Zmienne
+//
+	
+class DUSZEK
+{
+	public:
+		int x = 0,y;
+		ALLEGRO_BITMAP *obraz;
+	void rysuj (x,y,obraz,obraz1)
+	{
+		al_draw_bitmap(x,y, obraz);
+
+	}
+	private:
+	
+	void idzdo (int x, y;)
+	{
+		x++;
+		y =100;
+	
+	}
+	
+	
+	DUSZEK duszek1;
+	
+	x.duszek1 = 100;
+	y.duszek1 = 100;
+	obraz.duszek1 = al_load_bitmap("duszek.png");
+
+	if (obraz.duszek1 == NULL) {
+		cout << "Nie udalo sie zaladowac pliku." << endl;
+		exit(1);	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+//
+// Rysowanie planszy
+//
+
+void rysuj_plansze()
+{
+   duszek1.ryszuj();
+}
+
+//
+// Mechanika gry
+//
+
+void aktualizuj_plansze()
+{
+	
+}
+
+//
+// Reakcja na akcje graczy
+//
+
+void co_robia_gracze()
+{
+}
+
+
+void menu() {
+
+
+
+
+}
+
+
+/****************************************
+ * Kod poniżej jest w miarę generyczny  *
+ ****************************************/
+ 
+const float FPS = 60;       // Frames Per Second
+
+
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+ALLEGRO_TIMER *timer = NULL;
+
+int init()
+{
+    if(!al_init()) {
+        cerr << "Błąd podczas inicjalizacji allegro." << endl;
+        return -1;
+    }
+  
+    if (!al_init_primitives_addon()) {
+        cerr << "Błąd podczas inicjalizacji dodatku 'primitives'." << endl;
+        return -1;
+    }
+    
+    if (!al_install_keyboard()) {
+        cerr << "Błąd podczas inicjalizacji klawiatury." << endl;
+        return -1;
+    }
+
+    if (!al_install_mouse()) {
+        cerr << "Błąd podczas inicjalizacji myszy." << endl;
+        return -1;
+    }
+  
+    timer = al_create_timer(1.0 / FPS);
+    if(!timer) {
+        cerr << "Błąd podczas inicjalizacji zegara." << endl;
+        return -1;
+    }
+ 
+    display = al_create_display(screen_w, screen_h);
+    if(!display) {
+        cerr << "Błąd podczas inicjalizacji ekranu." << endl;
+        al_destroy_timer(timer);
+        return -1;
+    }
+
+    event_queue = al_create_event_queue();
+    if(!event_queue) {
+        cerr << "Błąd podczas inicjalizacji kolejki zdarzeń." << endl;
+        al_destroy_display(display);
+        al_destroy_timer(timer);
+        return -1;
+    }
+   
+    al_register_event_source(event_queue, al_get_display_event_source(display));  
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));  
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_clear_to_color(al_map_rgb(0,0,0));
+  
+    al_flip_display();  
+    al_start_timer(timer);
+
+    return 0;
+}
+
+int main(int argc, char ** argv)
+{
+    if (init() != 0) {
+        cerr << "Inicjalizacja nie powiodła się." << endl;
+        return -1;
+    }
+ 
+
+    bool przerysuj = true;
+    bool wyjdz = false;  
+
+    //
+    // Event loop - główna pętla programu
+    //
+        
+    while(!wyjdz)
+    {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+
+        if(ev.type == ALLEGRO_EVENT_TIMER) {
+            //
+            // minęła 1/60 (1/FPS) część sekundy
+            //
+            przerysuj = true;
+
+            co_robia_gracze();
+
+            aktualizuj_plansze();
+
+        } else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            key[ev.keyboard.keycode] = true;
+        } else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+            key[ev.keyboard.keycode] = false;
+            
+            if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+                wyjdz = true;
+            }
+        }
+
+        if(przerysuj && al_is_event_queue_empty(event_queue)) {
+            przerysuj = false;
+
+            rysuj_plansze();
+
+            al_flip_display();
+         }    
+    }
+
+    return 0;
+}
