303320fb8fae69f80164093478cdf6a2e2087d50
filipgrzechnik1@gazeta.pl <amn@mimuw.edu.pl>
2014-10-03 15:47:34 +0200

    Poprawione znaki konca linii

--- zajecia/Michalisko/dnitygodnia.cpp
+++ zajecia/Michalisko/dnitygodnia.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-cout <<"
-Poniedziałek
-Wtorek
-środa 
-Czwartek
-Piątek
-Sobota
-Niedziela";
+cout <<""
+"Poniedziałek\n"
+"Wtorek\n"
+"środa\n" 
+"Czwartek\n"
+"Piątek\n"
+"Sobota\n"
+"Niedziela\n";
 
 return 0;
 }
