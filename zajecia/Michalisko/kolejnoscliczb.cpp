aeb9b792a71be472648df57f6969feaaafc30cac
Michał Szymański <michal@ignis.net.pl>
2014-10-04 18:55:42 +0200

    Zadanie kolejnościliczb.cpp

---
+++ zajecia/Michalisko/kolejnoscliczb.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a, b, c;
+cout <<"Liczba pierwsza:";
+cin >> a;
+cout <<"Liczba druga:";
+cin >> b;
+cout <<"Liczba trzecia:";
+cin >> c;
+cout <<a;
+cout <<b;
+cout <<c;
+cout <<c;
+cout <<b;
+cout <<a;
+
+}
