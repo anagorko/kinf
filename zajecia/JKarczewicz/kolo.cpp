eb206c7234fedd6213b5a9d43a8c96bae51171f8
bkula <gabimurawski@wp.pl>
2014-10-10 16:58:18 +0200

    kolo.cpp

---
+++ zajecia/JKarczewicz/kolo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a;
+cout << "Podaj promień koła: ";
+cin >> a;
+cout << "Pole tego koła to " << 3.14159265359*(a*a) << "\n" ;
+cout << "Obwód tego koła to " << (2*3.14159265359)*a << "\n" ;
+return 0;
+}
+
