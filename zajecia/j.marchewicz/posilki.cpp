80f5e8260a0a909fe31fb413048b25881a4e23ba
Janek <jan.marchewicz@gmail.com>
2014-10-16 22:25:15 +0200

    Zadanie posilki

---
+++ zajecia/j.marchewicz/posilki.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int i; 
+	cout << "Podaj aktualną godzinę: " << endl;
+	cin >> i;
+	if ( i == 6)
+	{
+		cout << "Bajtuś teraz je." << endl;
+	}
+	else if ( i == 12)
+	{
+		cout << "Bajtuś teraz je." << endl;
+	}
+	else if ( i == 18)
+	{
+		cout << "Bajtuś teraz je." << endl;
+	}
+	else
+	{
+		cout << "Bajtuś teraz NIE JE." << endl;
+	}
+}
