ca684eae44c8a0541fdbce307db1dd5702e93643
pnagorko <michal@ignis.net.pl>
2014-10-17 17:00:47 +0200

    Nidokończone zadanie patyczki.cpp

---
+++ zajecia/Michalisko/patyczki.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int a, b, c;
+cin >> a >> b >> c;
+if (a+b>c && c+b>b && b+c>a){
+cout << "Morzesz ułorzyć trójkąt:";
+
+}
+if (a==b && a==c){
+cout << "rónoboczny"
+}
+
+}
+
+
+
