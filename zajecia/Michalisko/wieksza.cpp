f0adc190e2fcb5a1b1616edc22b6929f1213a5b5
pnagorko <michal@ignis.net.pl>
2014-10-17 16:59:22 +0200

    Program wieksza.cpp

---
+++ zajecia/Michalisko/wieksza.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a, b;
+cout << "Pierwsza liczba: ";
+cin >> a;
+cout << "Druga liczba: ";
+cin >> b;
+if (a>b){
+cout << a <<endl << b <<endl;
+}
+if (a<b){
+cout << b <<endl << a <<endl;
+}
+if (a==b){
+cout << "Twoje liczby są równe\n";
+}
+
+}
