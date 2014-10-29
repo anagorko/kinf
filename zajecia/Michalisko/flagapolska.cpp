099088761f38e575046d17f4285cdad7fe48faad
Michał Szymański <michal@ignis.net.pl>
2014-10-29 20:24:32 +0100

    Nieudolna pruba zrobienia zadania flagapolska.cpp

---
+++ zajecia/Michalisko/flagapolska.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a, h, c;
+cout << "Podaj szerokość:";
+cin >> a;
+cout << "Podaj wysokość:";
+cin >> h;
+for (int b=0; b<(h/2); b++){
+cout << (a*4);
+}
+
+}
