db7a288eeb419df76c0c9b105e22541b10133d35
Janek <jan.marchewicz@gmail.com>
2014-12-05 16:20:50 +0100

    obwod wielokata

---
+++ zajecia/j.marchewicz/obw.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int liczba_w; //liczba wierzcholkow
+	int dlugosc_b; //dlugosc boku
+	cin >> liczba_w;
+	cin >> dlugosc_b;
+	cout << liczba_w * dlugosc_b << endl;
+}
