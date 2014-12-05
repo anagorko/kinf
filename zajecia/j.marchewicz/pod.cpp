bc05dcaf2004f421a124a1e7bf60eae1927e4073
Janek <jan.marchewicz@gmail.com>
2014-12-05 16:30:35 +0100

    NIE wiem dlaczego nie działa

---
+++ zajecia/j.marchewicz/pod.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	long long int N;
+	cin >> N;
+	if (N % 100 == 0)
+	{
+		cout << "TAK" << endl;
+	}
+	else
+	{
+	cout << "NIE" << endl;
+	}
+}
