64d45fb5ad5ddd73871c0ae375ddd09f70432d32
Michal Szymanski <micha@ignis.net.pl>
2016-01-19 17:01:10 +0100

    Mocno niedokończone szachy

---
+++ zajecia/Michalisko/szachy.cpp
@@ -0,0 +1,20 @@
+#include<iostram>
+using namespace std;
+
+int main(){
+	int a, b, c, d;
+	cin >> a >> c;
+	int s[a][b];
+	a=b;
+	c=d;
+	while(b!=0){
+		while(d!=0){
+		cin >> s[a][c];
+		c--;
+		}
+	c=d;
+	b--;
+	}
+	a=b;
+    c=d;
+}
