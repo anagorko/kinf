72607b8ec9e1aea5b1fbb4e19e5cd7c748c1b14f
Michal Szymanski <michal@ignis.net.pl>
2016-03-22 07:55:05 +0100

    Już działający prgram molar mass

--- zajecia/Michalisko/uva/molar_mass.cpp
+++ zajecia/Michalisko/uva/molar_mass.cpp
@@ -10,40 +10,78 @@ int main(){
 		cin >> in;
 		int dl;
 		dl= in.length();
-		float a=0.0,b=0.0;
-		for(int j=0;j>=dl;j++){
-			if(in[j]==1)
-				a=a*in[j];
-				cout << "jeden";
-			if(in[j]=='2')
-				a=a*in[j];
-				cout << "dwa";
-			if(in[j]=='3')
-				a=a*in[j];
-			if(in[j]=='4')
-				a=a*in[j];
-			if(in[j]=='5')
-				a=a*in[j];
-			if(in[j]=='6')
-				a=a*in[j];
-			if(in[j]=='7')
-				a=a*in[j];
-			if(in[j]=='8')
-				a=a*in[j];
-			if(in[j]=='9')
-				a=a*in[j];
+		float a=0.0,b=0.0, m=0.0;
+		int f;
+		for(int j=0;j<=dl;j++){
+			if(in[j]=='1' && f==0){
+				a=a*1;
+				f++;}
+			else if(in[j]=='1' && f==1)
+				a=a*10+(1*m);
+			else if(in[j]=='2' && f==0){
+				a=a*2;
+				f++;}
+			else if(in[j]=='2' && f==1)
+				a=a*10+(2*m);
+			else if(in[j]=='3' && f==0){
+				a=a*3;
+				f++;}
+			else if(in[j]=='3' && f==1)
+				a=a*10+(3*m);
+			else if(in[j]=='4' && f==0){
+				a=a*4;
+				f++;}
+			else if(in[j]=='4' && f==1)
+				a=a*10+(4*m);
+			else if(in[j]=='5' && f==0){
+				a=a*5;
+				f++;}
+			else if(in[j]=='5' && f==1)
+				a=a*10+(5*m);
+			else if(in[j]=='6' && f==0){
+				a=a*6;
+				f++;}
+			else if(in[j]=='6' && f==1)
+				a=a*10+(6*m);
+			else if(in[j]=='7' && f==0){
+				a=a*7;
+				f++;}
+			else if(in[j]=='7' && f==1)
+				a=a*10+(7*m);
+			else if(in[j]=='8' && f==0){
+				a=a*8;
+				f++;}
+			else if(in[j]=='8' && f==1)
+				a=a*10+(8*m);
+			else if(in[j]=='9' && f==0){
+				a=a*9;
+				f++;}
+			else if(in[j]=='9' && f==1)
+				a=a*10+(9*m);
 			else{
-				if(in[j]=='C')
+				if(in[j]=='C'){
+					b=b+a;
 					a=12.01;
-				if(in[j]=='H')
+					m=a;
+					f=0;}
+				if(in[j]=='H'){
+					b=b+a;
 					a=1.008;
-				if(in[j]=='O')
+					m=a;
+					f=0;}
+				if(in[j]=='O'){
+					b=b+a;
 					a=16.0;
-				if(in[j]=='N')
+					m=a;
+					f=0;}
+				if(in[j]=='N'){
+					b=b+a;
 					a=14.01;
+					m=a;
+					f=0;}
 			}
-			b=b+a;
+			
 		}
-		cout << b;
+		cout << b+a << endl;
 	}
 }
